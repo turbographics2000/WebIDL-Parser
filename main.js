503b76c19770f7219a3277a2ee4af59a7da88d84
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-04-19 08:57:42 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-fetch('https://www.w3.org/TR/tr-groups-all')
-    .then(res => res.text())
-    .then(resText => {
-        var domParser = new DOMParser();
-        var dom = domParser.parseFromString(resText, 'text/html');
-        console.log(WebIDLParse(dom));
-    })
-    .catch(ex => console.log('fetch error', ex));
