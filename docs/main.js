7bec6bbea034a3978c5924066304339e44246ce2
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-04-19 08:32:58 +0900

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@ fetch('https://www.w3.org/TR/tr-groups-all')
     .then(res => res.text())
     .then(resText => {
         var domParser = new DOMParser();
-        var dom = domParser.parseFromString(resText);
+        var dom = domParser.parseFromString(resText, 'text/html');
         WebIDLParse(dom);
     })
     .catch(ex => console.log('fetch error', ex));
