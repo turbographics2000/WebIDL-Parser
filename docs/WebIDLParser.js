8c93fa1763df86e9a8022a20e8f1b4e240ebbb92
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-04-19 20:55:51 +0900

    Add files via upload

--- docs/WebIDLParser.js
+++ docs/WebIDLParser.js
@@ -100,7 +100,8 @@ function WebIDLParse(doc) {
                     break;
                 case 'Callback':
                     memberParse(groupElm, groupItemData, 'Callback');
-                    paramParse(groupElm, groupData);
+                    var cbParams = paramParse(groupElm);
+                    if(cbParams) groupData.params = cbParams;
                     break;
                 case 'Enum':
                     groupElm.querySelectorAll('.idlEnumItem').forEach(item => {
