cf1a92bde7d9e72d2550ea3f910f1397151a6066
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-04-24 20:56:22 +0900

    commit

--- docs/CSCodeGenerator.js
+++ docs/CSCodeGenerator.js
@@ -212,7 +212,7 @@ function generateCS(parseData, classStructs, arrayToList) {
 
         for (var i = 0, il = method.param_pattern.length; i < il; i++) {
             var params = method.param_pattern[i];
-            if(!params) debugger;
+            if(!params) debugger; 
             if(!params.map) debugger;
             var paramString = params.map(pt => {
                 var ret = `, ${pt.type} ${pt.name}`;
